65855edf30517268973c4b16c6d809b8c6d7a23b
sebastianaa <symbolic.92@gmail.com>
2024-10-08 23:10:57 -0500

    Adición de archivos

--- Clase_4.py
+++ Clase_4.py
@@ -49,19 +49,33 @@ x = np.ones(n)
 
 start = 3
 y = np.arange(start,3 * (n+1),3)
-#print(y)
+#print("Esto es y",y)
 
 y2 = np.round(15 * np.sin(y))
-#print(y2)
+#print("Esto es y2",y2)
 
 # indices = np.where(y2 > 6)
 # contar_cincos = np.sum(y2==5)
 # print("Coso",indices)
 # print("Cosos",contar_cincos)
 
+M = np.array([y,y2])
+# print(M)
 
-M = np.vstack([y,y2])
-print(M)
+# M2 = np.array([y2.T, y2.T])
+# print(M2)
 
-M2 = np.column_stack([y.T, y2.T])
-print(M2)
+# M3 = np.concatenate((M,[x]),axis=0 )
+# print(M3)
+
+# M4 = np.concatenate((M2,[x]),axis=0)
+# print(M4)
+
+
+coso2 = M>5
+coso3 = np.all(M > 5 , axis = 0)
+print(coso3)
+coso4 = np.where(np.all(M>6, axis=0))
+print(coso4)
+tales = M[:, coso4]
+print(tales)
